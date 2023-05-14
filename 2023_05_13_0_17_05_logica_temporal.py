2f9f811456b4eb7d65b503db872fd37d9c25df94
Alnitakr51 <gadieljj34@gmail.com>
2023-05-14 02:30:02 +0200

    Otras Logicas

---
+++ 2023_05_13_0_17_05_logica_temporal.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 14 01:18:34 2023
+
+@author: Gadiel Jimenez
+"""
+
+# Definición de las variables
+p = True
+q = False
+
+# Definición de la fórmula temporal
+formula = (not p) or (p and q)
+
+# Evaluación de la fórmula
+result = formula
+
+# Imprimir el resultado
+print(f"El resultado de la fórmula es: {result}")
